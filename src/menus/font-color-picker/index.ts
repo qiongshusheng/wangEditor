32a87a3b698329b35904d65e61f9f171fa5ddf2c
clinfc <1962109568@qq.com>
2021-02-01 23:51:16 +0800

    refactor: 提取颜色选择器菜单的公共内容为基础菜单

--- src/menus/font-color-picker/index.ts
+++ src/menus/font-color-picker/index.ts
@@ -4,83 +4,24 @@
  */
 
 import Editor from '../../editor'
-import ColorPicker from '../../editor/color-picker'
 import $ from '../../utils/dom-core'
-import Menu from '../menu-constructors/Menu'
+import ColorPickerMenu from '../menu-constructors/ColorPickerMenu'
 
-export default class FontColorPicker extends Menu {
-    public picker: ColorPicker
-
+export default class FontColorPicker extends ColorPickerMenu {
     public constructor(editor: Editor) {
         const $elem = $(
             `<div class="w-e-menu" data-title="文字颜色">
-                <i class="w-e-icon-pencil2" ref="icon"></i>
+                <i class="w-e-icon-pencil2"></i>
             </div>`
         )
-        super($elem, editor)
-
-        // i18next
-        function t(name: string) {
-            return editor.i18next.t(`colorPicker.${name}`)
+        const config = {
+            space: 'font',
+            alpha: false,
+            done: (color: string) => {
+                this.command(color)
+            },
         }
-
-        // 初始化颜色选择器
-        this.picker = ColorPicker.create(
-            Object.assign(
-                { ...editor.config.colorPicker },
-                {
-                    space: 'font',
-                    alpha: false,
-                    append: $elem,
-                    builtInTitle: t('内置颜色列表'),
-                    historyTitle: t('最近使用的颜色'),
-                    customTitle: t('自定义颜色列表'),
-                    text: {
-                        toPalette: t('调色板'),
-                        toSelect: t('颜色列表'),
-                        done: t('确定'),
-                        cancel: t('取消'),
-                        empty: t('无'),
-                    },
-                    done: (color: string) => {
-                        this.command(color)
-                    },
-                }
-            )
-        )
-
-        // 配置颜色选择器的 z-index
-        this.picker.css('z-index', editor.zIndex.get('panel'))
-
-        // 显示颜色选择器的回调函数
-        let show = () => {
-            const height = $elem.getBoundingClientRect().height
-            this.picker.css('margin-top', `${height}px`)
-            this.picker.show()
-            // 重写 show 函数
-            show = () => this.picker.show()
-        }
-
-        // 绑定菜单的鼠标悬浮事件
-        $elem.$ref('icon').on('mouseenter', () => {
-            if (editor.selection.getRange() !== null) {
-                editor.txt.eventHooks.dropListMenuHoverEvents.forEach(fn => fn())
-                $elem.css('z-index', editor.zIndex.get('menu'))
-                show()
-            }
-        })
-
-        // 隐藏颜色选择器的回调函数
-        const hide = () => {
-            $elem.css('z-index', 'auto')
-            this.picker.hide()
-        }
-
-        // 将函数加入对应的钩子中
-        this.editor.txt.eventHooks.clickEvents.push(hide)
-        this.editor.txt.eventHooks.menuClickEvents.push(hide)
-        this.editor.txt.eventHooks.toolbarClickEvents.push(hide)
-        this.editor.txt.eventHooks.dropListMenuHoverEvents.push(hide)
+        super($elem, editor, config)
     }
 
     /**
